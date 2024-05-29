d2d9a62840201aa4f38734d805fa24e71228e305
OptimusQuidFacit <chimkaemewiseman@gmail.com>
2024-05-29 14:21:38 +0100

    add sitemap and robots.txt

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://yourwebsite.com', // Replace with your site's URL
+    // generateRobotsTxt: true, // (optional) Generate a robots.txt file
+  };
+  
