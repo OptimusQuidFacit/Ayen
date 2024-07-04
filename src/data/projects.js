99ac6aad22f941738a98d70adf32a245ddf08d36
Kemmy <116153365+OptimusQuidFacit@users.noreply.github.com>
2024-07-04 12:20:34 +0100

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,7 +6,7 @@ export const Project1= {
 
 export const project2 ={
     title: "Movie Screening Tour",
-    body: ` The African Youths in Energy Network  movie screening tour Project is aimed  to raise awareness about the critical issue of energy poverty. The tour featured the film  &quot;Switch On&quot;, a powerful movie based on true events that highlights the struggles faced by individuals and communities lacking access to basic energy services.
+    body: ` The African Youths in Energy Network  movie screening tour Project is aimed  to raise awareness about the critical issue of energy poverty. The tour featured the film  "Switch On", a powerful movie based on true events that highlights the struggles faced by individuals and communities lacking access to basic energy services.
             The objective is to educate viewers, especially youth, about the causes and impacts of energy poverty while inspiring them to take action to support efforts promoting universal access to clean, sustainable energy. After each screening, a panel discussion with energy experts will provide further insights and resources.
             By bringing this important film to Youths across the continent, AYEN aims to spark conversation, understanding, and mobilize youth to join the fight against energy poverty affecting millions worldwide. The tour represents a platform to facilitate dialogue and drive positive change through access to energy for all.`
 }
